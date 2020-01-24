ec2841e7896b505dcce1e86d090280ff5b9c7bbb
Matt Pahuta <mpahuta@uw.edu>
2020-01-24 10:52:17 -0800

    update jquery effects

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-
 $(document).ready(function() {
+  
   var questionSide = 'https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png';
   var answerSide = ' https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png';
   var magic8Ball = {};
   magic8Ball.answersArray = ['It is certain', 'Yes - Definitely', 'Nope', 'Most likely', 'Cannot predict now', 'My sources say no', 'Signs point to yes', 'Concentrate and ask again', 'Ask again later', 'Don\'t count on it', 'Very doubtful', 'As I see it, yes', 'Yeah, good luck with that'];
 
-  magic8Ball.askQuestion = function(question){
+  // Hide answer upon page load
+  $('#answer').hide();
+
+  magic8Ball.answerQuestion = function(question){
     // var randomNumber = Math.random();
     // var randomNumberForListOfAnswers = randomNumber * this.answersArray.length;
     // var randomIndex = Math.floor(randomNumberForListOfAnswers);
@@ -20,21 +23,20 @@ $(document).ready(function() {
     // console.log(question);
     // console.log(answer);
   };
-  // Hide answer upon page load
-  $('#answer').hide();
 
-  var onClick = function() {
-    $('#8ball').effect('shake');
-    $('#answer').hide();
+  // $('#answer').hide();
+
+  setTimeout(function() {
+    var onClick = function() {
+      var question = prompt('Ask a Yes or No Question!');
+      magic8Ball.answerQuestion(question);
+      $('#8ball').effect('shake');
+    };
+
+    $('#questionButton').click(onClick);
     $('#8ball').attr('src', questionSide);
-    // Wait before showing the prompt
-    setTimeout( function() {
-      var question = prompt("Ask a Yes/No Question!");
-      magic8Ball.askQuestion(question);
-    }, 500);
-  };
+  }, 500);
 
-  $('#questionButton').click(onClick);
 
 });
 
